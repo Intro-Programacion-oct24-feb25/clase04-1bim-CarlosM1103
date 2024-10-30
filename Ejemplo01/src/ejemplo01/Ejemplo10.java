4fa74b85bf60cf15016e3867ef360276efd9fcfb
CarlosM1103 <carlosmedinal2006@gmail.com>
2024-10-30 11:56:21 -0500

    Que onda

--- Ejemplo01/src/ejemplo01/Ejemplo10.java
+++ Ejemplo01/src/ejemplo01/Ejemplo10.java
@@ -11,12 +11,13 @@ package ejemplo01;
  *
  * @author reroes
  */
-public class Ejemplo05 {
+public class Ejemplo10 {
     public static void main(String[] args) {
         // Secuencia de escape \n y \t
-        String nombreEstudiante = "René Rolando";
-        String apellidoEstudiante = "Elizalde Solano";
-        int nacimiento = 1983;
+        String nombreEstudiante = "Carlos Daniel";
+        String apellidoEstudiante = "Medina Larreátegui";
+        int edad = 40;
+        
         
         /*System.out.println(nombreEstudiante+"\n"+apellidoEstudiante+ "\n"+ 
                 nacimiento);
@@ -26,7 +27,7 @@ public class Ejemplo05 {
         
         System.out.println(nombreEstudiante+"\n"+"\n"+apellidoEstudiante+ "\n"+
                 "\n"+ nacimiento); */
-        System.out.println(nombreEstudiante+"\t"+apellidoEstudiante);   
-        System.out.println(nombreEstudiante+"\n\n\t"+apellidoEstudiante);   
+        System.out.println(nombreEstudiante+"\n\t"+apellidoEstudiante+"\n\t\t"+edad);   
+       
     }
 }
